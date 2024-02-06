5ced360ee563fccd9791afee8e484cf9de79ef68
Sonal Jayasinghe <sonaldanindulk@gmail.com>
2024-02-06 09:28:36 +0530

    Working Face Detection

---
+++ app/components/LeftRightBalanc.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LeftRightBalanc = () => {
+  return (
+    <div>LeftRightBalanc</div>
+  )
+}
+
+export default LeftRightBalanc
--- app/page.js
+++ app/page.js
@@ -1,113 +1,191 @@
-import Image from "next/image";
+"use client";
+import React, { useRef, useEffect, useState } from "react";
+import * as tf from "@tensorflow/tfjs";
+import * as facemesh from "@tensorflow-models/facemesh";
+import Webcam from "react-webcam";
+import { drawMesh } from "./utilities.js";
+import { CiLocationArrow1 } from "react-icons/ci";
+
 
 export default function Home() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+  const [leftvalue, setLeftValue] = useState(0);
+  const [rightvalue, setRightValue] = useState(0);
+  const [normalizedValue, setNormalizedValue] = useState(0);
+  const [faceAngle, setFaceAngle] = useState(0);
+
+  const runFacemesh = async () => {
+    const net = await facemesh.load({
+      inputResolution: { width: 1280, height: 720 },
+      maxFaces: 1,
+      scale: 0.8,
+    });
+
+    setInterval(() => {
+      detect(net);
+    }, 10);
+  };
+
+  const initialXRef = useRef(null);
+
+  const calculateYawAndPitch = (landmarks) => {
+    if (initialXRef.current === null) {
+      initialXRef.current = landmarks[0][0];
+    }
+
+    const currentX = landmarks[0][0];
+    const deltaX = currentX - initialXRef.current;
+
+    const yawRotation = mapTo180Degrees(deltaX);
+    setFaceAngle(yawRotation);
+    return yawRotation;
+  };
+
+  const mapTo180Degrees = (x) => {
+    const scalingFactor = 90 / 100;
+    const degrees = x * scalingFactor;
+    return Math.min(Math.max(90 - degrees, 0), 180);
+  };
+
+  const audioBalancerUI = (yaw) => {
+    const normalizedYaw = yaw - 90;
+    const normalizedValue = normalizedYaw / 180;
+    setNormalizedValue(normalizedValue * 2);
+
+    const rightVolume = Math.max(0.5 - normalizedValue, 0) + 0.1;
+    const leftVolume = Math.max(0.5 + normalizedValue, 0) + 0.1;
+
+    setLeftValue(leftVolume.toFixed(3));
+    setRightValue(rightVolume.toFixed(3));
+  };
+
+  const detect = async (net) => {
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null
+    ) {
+      // Get Video Properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      // Set video width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      // Set canvas width
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      
+      const face = await net.estimateFaces(video);
+      var yaw;
+      try{
+       yaw = calculateYawAndPitch(face[0].scaledMesh);
+      } catch (e) {
+      }
+      
+      audioBalancerUI(yaw);
+
+      const ctx = canvasRef.current.getContext("2d");
+      requestAnimationFrame(() => {
+        drawMesh(face, ctx);
+      });
+    }
+  };
+
+  useEffect(() => {
+    runFacemesh();
+  }, [1]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div className="flex flex-col">
+        {/* Webcam and Canvas */}
+        <div className="flex mt-10">
+          <Webcam
+            ref={webcamRef}
+            style={{
+              position: "absolute",
+              marginLeft: "auto",
+              marginRight: "auto",
+              left: 0,
+              right: 0,
+              textAlign: "center",
+              zIndex: 9,
+              width: 640,
+              height: 480,
+              borderRadius: 20,
+            }}
+            mirrored={true}
+          />
+
+          <canvas
+            ref={canvasRef}
+            style={{
+              position: "absolute",
+              marginLeft: "auto",
+              marginRight: "auto",
+              left: 0,
+              right: 0,
+              textAlign: "center",
+              zIndex: 9,
+              width: 640,
+              height: 480,
+              transform: "scaleX(-1)", // Apply horizontal mirroring
+            }}
+          />
+        </div>
+
+        <div>
+          <div className="justify-center inline-block ml-32 -mt-10">
+            {/* Audio Channels */}
+            <div className="flex flex-col m-10 bg-gray-900 justify-center items-center w-52 h-80 rounded-2xl gap-7">
+              <h3> Audio Channels Gain </h3>
+
+              <div className="flex gap-12">
+                <div className="items-center">
+                  <h3 className="text-center text-slate-400"> Left</h3>
+
+                  <div className="w-3 h-48 ml-4 bg-green-500 rounded-full overflow-hidden mt-2">
+                    <div
+                      style={{ height: `${(1 - leftvalue) * 100}%` ,transitionTimingFunction: "ease-in-out", transition: "0.2s"}}
+                      className="h-full bg-gray-800"
+                    ></div>
+                  </div>
+                </div>
+
+                <div className="items-center">
+                  <h3 className="text-center text-slate-400"> Right</h3>
+
+                  <div className="w-3 h-48 ml-4 bg-green-500 rounded-full overflow-hidden mt-2">
+                    <div
+                      style={{ height: `${(1 - rightvalue) * 100}%`, transitionTimingFunction: "ease-in-out", transition: "0.2s"}}
+                      className="h-full bg-gray-800"
+                    ></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            {/* Rotation Channels */}
+
+            <div className="flex-col m-10 bg-gray-900 justify-center items-center flex w-52 h-52 rounded-2xl gap-2">
+              <h3> Face Rotation </h3>
+              <CiLocationArrow1
+                size={100}
+                style={{ rotate: `${-135 + faceAngle}deg`, transitionTimingFunction: "ease-in-out", transition: "0.2s"}}
+                className="mt-4"
+              />
+              <h4 className="text-center text-slate-400">
+                {" "}
+                {Math.round(faceAngle)}°
+              </h4>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   );
 }
